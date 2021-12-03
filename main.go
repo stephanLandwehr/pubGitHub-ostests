2b4c34c2624b83d8d626af73eda01ba47e744e1f
stephanLandwehr <72545596+stephanLandwehr@users.noreply.github.com>
2021-12-03 08:59:57 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/nodir", func(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello W.")})
 	http.ListenAndServe(":8082", nil)
 }
