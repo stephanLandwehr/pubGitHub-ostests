53287cc3d194badeb2a0e5b82f6ff39be18f5736
stephanLandwehr <72545596+stephanLandwehr@users.noreply.github.com>
2021-12-03 09:08:22 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	main2 "https://github.com/stephanLandwehr/os-test/openshift-tests/openshift-test/main2.go"
 )
 
 func main() {
 	http.HandleFunc("/nodir", func(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello W.")})
 	http.ListenAndServe(":8082", nil)
+	main2.main()
 }
 
 
