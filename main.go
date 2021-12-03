06123ec6111b6e956429717192b7d8259f88626a
Stephan Landwehr <stephan.landwehr@ibm.com>
2021-12-03 10:27:35 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
-	main2 "github.com/stephanLandwehr/os-test/openshift-tests/openshift-test/main2.go"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/nodir", func(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello W.")})
-	main2.main()
+	randInt := 0
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %d", randInt)
+	})
+	http.ListenAndServe(":8082", nil)
+	s1 := rand.NewSource(time.Now().UnixNano())
+	for i := 0; i < 100; i++ {
+		randInt = rand.New(s1).Intn(100)
+		time.Sleep(5 * time.Second)
+	}
 }
-
-
